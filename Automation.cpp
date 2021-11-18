841fb896939a38d74637056524045b80f3332e4c
Franz Höpfinger <franz.hoepfinger@ms-muc.de>
2021-11-18 10:18:37 +0100

    THREE_POSITION_SWITCH

--- Automation.cpp
+++ Automation.cpp
@@ -29,3 +29,50 @@ void INTERLOCK::operator ()(void) {
 	Q2 = I2 && !T1.Q;
 
 }
+
+
+
+
+
+
+
+void THREE_POSITION_SWITCH::operator ()(void) {
+
+	if(!I1 && !I2){
+		State = Off;
+		return; //nothing to do !
+	}
+	// here we are NOT Off or we are Off but a Input is there.
+
+
+	switch (State) {
+	case Off:
+		// if we are Off, we go in Momentary.
+		if( I1 && !I2){
+			State = Momentary_forward_up_right;
+		}
+		if(!I1 && I2){
+			State = Momentary_backward_down_left;
+		}
+		break;
+
+	case Momentary_forward_up_right:
+		if(I1){
+			State = held_forward_up_right;
+		}
+		break;
+
+	case Momentary_backward_down_left:
+		if(I2){
+			State = held_backward_down_left;
+		}
+		break;
+
+	default:
+	case held_forward_up_right:
+	case held_backward_down_left:
+		//NOOP
+		break;
+	}
+
+}
--- Automation.h
+++ Automation.h
@@ -37,4 +37,53 @@ private:
 
 
 
+
+enum THREE_POSITION_SWITCH_v1{
+	Off                          = 0,
+	Momentary_forward_up_right   = 1,
+	held_forward_up_right        = 2,
+	Momentary_backward_down_left = 4,
+	held_backward_down_left      = 8
+};
+
+/**
+ * \brief
+
+	Three-Position	Switch,		(returning	to	centre	position)
+	(Momentary	Single	Pole,	Three	Position,	Centre	Off)
+	Value	1:
+	0	=	Off
+	1	=	Momentary	=	forward,	up	or	right
+	2	=	held	forward,	up,	or	right
+	4	=	Momentary	=	backward,	down	or	left
+	8	=	held	backward,	down,	or	left
+	Value	2:
+	Number	of	transitions	from	Off	to	not	Off	since	power
+	up	(Momentary	to	held	is	not	counted).	Overflows
+	from	FFFF(16)	to	0.
+
+ *
+ */
+class THREE_POSITION_SWITCH
+{
+	public:
+	//VAR_INPUT
+	bool I1 = false;		/* input 1 forward,	up or right */
+	bool I2 = false;		/* input 2 backward, down or left */
+	//VAR_OUTPUT
+	THREE_POSITION_SWITCH_v1 State = Off;		/* output 1 */
+	uint16_t Value2 = 0;		/* output 2 */
+	//call
+	void operator()(void);  /*  */
+};
+
+
+
+
+
+
+
+
+
+
 #endif /* COMPONENTS_PLCLIB_AUTOMATION_H_ */
