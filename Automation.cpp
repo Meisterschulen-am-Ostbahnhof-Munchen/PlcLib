7aaff90e04d5263824d0f2c82a39595524a0ca84
Franz Höpfinger <franz.hoepfinger@ms-muc.de>
2021-11-18 10:24:55 +0100

    formatting !

--- Automation.cpp
+++ Automation.cpp
@@ -19,14 +19,14 @@
 
 void INTERLOCK::operator ()(void) {
 
-	/* the input signal have a run delay to lockout the other input */
-	T1.PT = TL;
-	T2.PT = TL;
-	T1(I1);
-	T2(I2);
+    /* the input signal have a run delay to lockout the other input */
+    T1.PT = TL;
+    T2.PT = TL;
+    T1(I1);
+    T2(I2);
 
-	Q1 = I1 && !T2.Q;
-	Q2 = I2 && !T1.Q;
+    Q1 = I1 && !T2.Q;
+    Q2 = I2 && !T1.Q;
 
 }
 
@@ -38,41 +38,15 @@ void INTERLOCK::operator ()(void) {
 
 void THREE_POSITION_SWITCH::operator ()(void) {
 
-	if(!I1 && !I2){
-		State = Off;
-		return; //nothing to do !
-	}
-	// here we are NOT Off or we are Off but a Input is there.
-
-
-	switch (State) {
-	case Off:
-		// if we are Off, we go in Momentary.
-		if( I1 && !I2){
-			State = Momentary_forward_up_right;
-		}
-		if(!I1 && I2){
-			State = Momentary_backward_down_left;
-		}
-		break;
-
-	case Momentary_forward_up_right:
-		if(I1){
-			State = held_forward_up_right;
-		}
-		break;
-
-	case Momentary_backward_down_left:
-		if(I2){
-			State = held_backward_down_left;
-		}
-		break;
-
-	default:
-	case held_forward_up_right:
-	case held_backward_down_left:
-		//NOOP
-		break;
-	}
+                                         if(!I1 && !I2){        State = Off;        return;      } //Off is Off. nothing else.
+    switch (State) {
+    case Off:                            if( I1 && !I2){  State = Momentary_forward_up_right;    }			//if we was off, we go mom.
+                                         if(!I1 &&  I2){  State = Momentary_backward_down_left;  } break;
+    case Momentary_forward_up_right:     if( I1       ){  State = held_forward_up_right;         } break;  //held
+    case Momentary_backward_down_left:   if(        I2){  State = held_backward_down_left;       } break;  //held
+    default:
+    case held_forward_up_right:
+    case held_backward_down_left:        /*NOOP*/                                                  break; //nothing to do.
+    }
 
 }
