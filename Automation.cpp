f3afe7e270c814ab3e6bad19ecfad54ac7414069
Franz Höpfinger <franz.hoepfinger@ms-muc.de>
2021-11-18 19:01:51 +0100

    ..

--- Automation.cpp
+++ Automation.cpp
@@ -64,3 +64,32 @@ void THREE_POSITION_VALVE::operator ()(void) {
 
 
 }
+
+void FOUR_POSITION_SWITCH::operator ()(void) {
+
+    /* any case */                                                 if(!I1 && !I2) {  State = FOUR_POSITION_SWITCH_v1::Off;        return;               }               //Off is Off. nothing else.
+    switch (State) { /* in case */                                                                                                                                       //
+    case FOUR_POSITION_SWITCH_v1::Off:                            if( I1 && !I2) {  State = FOUR_POSITION_SWITCH_v1::Momentary_forward_up_right;   Value2++; }         //if we was off, we go mom.
+                                                                   if(!I1 &&  I2) {  State = FOUR_POSITION_SWITCH_v1::Momentary_backward_down_left; Value2++; } break;  //if we was off, we go mom.
+    case FOUR_POSITION_SWITCH_v1::Momentary_forward_up_right:     if( I1       ) {  State = FOUR_POSITION_SWITCH_v1::held_forward_up_right;                  } break;  // held
+    case FOUR_POSITION_SWITCH_v1::Momentary_backward_down_left:   if(        I2) {  State = FOUR_POSITION_SWITCH_v1::held_backward_down_left;                } break;  // held
+    case FOUR_POSITION_SWITCH_v1::held_forward_up_right:          if(!I1       ) {  State = FOUR_POSITION_SWITCH_v1::Off;                                    } break;  // only exit if Button is no more pressed.
+    case FOUR_POSITION_SWITCH_v1::held_backward_down_left:        if(       !I2) {  State = FOUR_POSITION_SWITCH_v1::Off;                                    } break;  // only exit if Button is no more pressed.
+    default:                                            {  State = FOUR_POSITION_SWITCH_v1::Off;                                    } break;  // ERROR
+    }
+
+}
+
+void VALVE_WITH_FLOAT::operator ()(void) {
+
+	switch (State){
+	case FOUR_POSITION_SWITCH_v1::Off:                             Q1 = false; Q2 = false; break;
+	case FOUR_POSITION_SWITCH_v1::Momentary_forward_up_right:      Q1 = true;  Q2 = false; break;
+	case FOUR_POSITION_SWITCH_v1::held_forward_up_right:           Q1 = true;  Q2 = false; break;
+	case FOUR_POSITION_SWITCH_v1::Momentary_backward_down_left:    Q1 = false; Q2 = true;  break;
+	case FOUR_POSITION_SWITCH_v1::held_backward_down_left:         Q1 = false; Q2 = true;  break;
+	default:                              Q1 = false; Q2 = false; break;
+	}
+
+
+}
--- Automation.h
+++ Automation.h
@@ -89,30 +89,14 @@ class THREE_POSITION_SWITCH
 	void operator()(void);  /*  */
 };
 
-/**
- * \brief
 
-	Three-Position	Switch,		(returning	to	centre	position)
-	(Momentary	Single	Pole,	Three	Position,	Centre	Off)
-	Value	1:
-	0	=	Off
-	1	=	Momentary	=	forward,	up	or	right
-	2	=	held	forward,	up,	or	right
-	4	=	Momentary	=	backward,	down	or	left
-	8	=	held	backward,	down,	or	left
-	Value	2:
-	Number	of	transitions	from	Off	to	not	Off	since	power
-	up	(Momentary	to	held	is	not	counted).	Overflows
-	from	FFFF(16)	to	0.
-
- *
- */
 class FOUR_POSITION_SWITCH
 {
 	public:
 	//VAR_INPUT
-	bool I1 = false;		/* input 1 forward,	up or right */
-	bool I2 = false;		/* input 2 backward, down or left */
+	bool I1 = false;		/* input 1 forward,	   up or right */
+	bool I2 = false;		/* input 2 backward, down or left  */
+	bool I3 = false;		/* input 3 float  */
 	//VAR_OUTPUT
 	FOUR_POSITION_SWITCH_v1 State
 	    = FOUR_POSITION_SWITCH_v1::Off;		/* output 1 */
@@ -130,7 +114,7 @@ class THREE_POSITION_VALVE
 	    = THREE_POSITION_SWITCH_v1::Off;		/* Input 1 */
 	//VAR_OUTPUT
 	bool Q1 = false;		/* output 1 forward,	up or right */
-	bool Q2 = false;		/* output 2 backward, down or left */
+	bool Q2 = false;		/* output 2 backward, down or left  */
 	//call
 	void operator()(void);  /*  */
 };
@@ -144,8 +128,10 @@ class VALVE_WITH_FLOAT
 	FOUR_POSITION_SWITCH_v1 State
 	    = FOUR_POSITION_SWITCH_v1::Off;		/* Input 1 */
 	//VAR_OUTPUT
-	bool Q1 = false;		/* output 1 */
-	bool Q2 = false;		/* output 2 */
+	bool Q1 = false;		/* output 1                        forward,  up   or right */
+	bool Q2 = false;		/* output 2                        backward, down or left  */
+	bool Q3 = false;		/* output 3 float and safety Valve forward,  up   or right */
+	bool Q4 = false;		/* output 4 float and safety Valve backward, down or left  */
 	//call
 	void operator()(void);  /*  */
 };
