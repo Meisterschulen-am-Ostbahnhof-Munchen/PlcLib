1601e4bf4b50d9452f153493a6d0ee7057f580e5
Franz Höpfinger <f.hoepfinger@hr-agrartechnik.de>
2021-12-05 09:45:26 +0100

    FOUR_POSITION_TOF

--- AutomationTimer.cpp
+++ AutomationTimer.cpp
@@ -74,3 +74,58 @@ void THREE_POSITION_TOF::operator ()(void)
 		break;
 	}
 }
+
+void FOUR_POSITION_TOF::operator ()(void)
+{
+
+	int32_t tx;					/* internal variable */
+
+	/* read system timer */
+	tx = T_PLC_MS();
+
+	ET = tx - StartTime;
+	ESP_LOGV(TAG, "ET %i    PT %i", ET, PT);
+
+	switch (IN){
+	case FOUR_POSITION_SWITCH_v1::Off:
+		if (ET >= PT)
+		{
+			OUT = FOUR_POSITION_SWITCH_v1::Off;
+		}
+		break;
+	case FOUR_POSITION_SWITCH_v1::Momentary_forward_up_right:
+	case FOUR_POSITION_SWITCH_v1::Momentary_backward_down_left:
+		if (OUT != FOUR_POSITION_SWITCH_v1::Off)
+		{
+			ESP_LOGD(TAG, "TOF_1: cancel detected");
+			OUT = FOUR_POSITION_SWITCH_v1::Off;
+			IN  = FOUR_POSITION_SWITCH_v1::Off;
+		}
+		else
+		{
+			ESP_LOGD(TAG, "TOF_1: raising Edge detected");
+			//Start the Timer
+			StartTime = tx;
+			OUT = IN;
+		}
+		break;
+	case FOUR_POSITION_SWITCH_v1::held_forward_up_right:
+		if (OUT == FOUR_POSITION_SWITCH_v1::Momentary_forward_up_right)
+		{
+			ESP_LOGD(TAG, "TOF_1: raising Edge II detected");
+			OUT = IN;
+		}
+		break;
+	case FOUR_POSITION_SWITCH_v1::held_backward_down_left:
+		if (OUT == FOUR_POSITION_SWITCH_v1::Momentary_backward_down_left)
+		{
+			ESP_LOGD(TAG, "TOF_1: raising Edge II detected");
+			OUT = IN;
+		}
+		break;
+	default:
+		OUT = FOUR_POSITION_SWITCH_v1::Off;
+		IN  = FOUR_POSITION_SWITCH_v1::Off;
+		break;
+	}
+}
--- AutomationTimer.h
+++ AutomationTimer.h
@@ -14,17 +14,36 @@
 
 class THREE_POSITION_TOF
 {
-	public:
-	//VAR_INPUT
-	int32_t PT = 0;
-	THREE_POSITION_SWITCH_v1 IN	    = THREE_POSITION_SWITCH_v1::Off;		/* input 1 */
-	//VAR_OUTPUT
-	THREE_POSITION_SWITCH_v1 OUT	= THREE_POSITION_SWITCH_v1::Off;		/* output 1 */
-	int32_t ET = 0; 		/* elapsed time */
-	//call
-	void operator()(void);  /*  */
-	private:
-	int32_t StartTime = 0;	/* internal variable */
+    public:
+    //VAR_INPUT
+    int32_t PT = 0;
+    THREE_POSITION_SWITCH_v1 IN        = THREE_POSITION_SWITCH_v1::Off;        /* input 1 */
+    //VAR_OUTPUT
+    THREE_POSITION_SWITCH_v1 OUT    = THREE_POSITION_SWITCH_v1::Off;        /* output 1 */
+    int32_t ET = 0;         /* elapsed time */
+    //call
+    void operator()(void);  /*  */
+    private:
+    int32_t StartTime = 0;  /* internal variable */
+};
+
+
+
+
+
+class FOUR_POSITION_TOF
+{
+    public:
+    //VAR_INPUT
+    int32_t PT = 0;
+    FOUR_POSITION_SWITCH_v1 IN        = FOUR_POSITION_SWITCH_v1::Off;        /* input 1 */
+    //VAR_OUTPUT
+    FOUR_POSITION_SWITCH_v1 OUT        = FOUR_POSITION_SWITCH_v1::Off;        /* output 1 */
+    int32_t ET = 0;         /* elapsed time */
+    //call
+    void operator()(void);  /*  */
+    private:
+    int32_t StartTime = 0;    /* internal variable */
 };
 
 
