8fd215b6fb72167568ada496cf4e61cc49e21e23
Franz Höpfinger <franz.hoepfinger@ms-muc.de>
2021-12-05 17:15:20 +0100

    This test does not yet test anything. but it assures Type Safety !

---
+++ test/test_FOUR_POSITION.cpp
@@ -0,0 +1,22 @@
+/* test_PlcLib.c: Implementation of a testable component.
+
+
+*/
+
+
+#include "unity.h"
+#include "Automation_FOUR_POSITION.h"
+#include "AutomationTimer_FOUR_POSITION.h"
+
+TEST_CASE("Test PlcLib", "[PlcLib]")
+{
+	FOUR_POSITION_SWITCH SWITCH;
+	FOUR_POSITION_TOF    TIMER;
+	VALVE_WITH_FLOAT  VALVE;
+	TIMER.PT = 3000;
+
+
+}
+
+
+
