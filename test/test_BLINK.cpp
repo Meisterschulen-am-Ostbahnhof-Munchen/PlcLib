964f6f7c118264ff3776e029c67b4a87406eb7dc
Franz Höpfinger <franz.hoepfinger@ms-muc.de>
2021-12-05 19:05:10 +0100

    EXAMPLE_BLINK

---
+++ test/test_BLINK.cpp
@@ -0,0 +1,23 @@
+/* test_BLINK.cpp: Implementation of a testable component.
+
+
+*/
+
+
+#include "unity.h"
+#include "UtilLib.h"
+
+TEST_CASE("Test BLINK", "[PlcLib]")
+{
+    BLINK BLINK1;
+    BLINK1.TIMEHIGH = 500;
+    BLINK1.TIMELOW = 100;
+    BLINK1(true);
+    BLINK1(false);
+    //TODO implement the Rest of the Test.
+
+
+}
+
+
+
