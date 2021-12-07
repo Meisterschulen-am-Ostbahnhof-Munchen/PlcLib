0ff7193eea26bf93740a001f2faa94be09dfa299
Franz Höpfinger <franz.hoepfinger@ms-muc.de>
2021-12-07 09:56:02 +0100

    some first additions.

--- StandardLibObserved.cpp
+++ StandardLibObserved.cpp
@@ -21,6 +21,193 @@
 
 static const char * const TAG = "StandardLibObserved";
 
+
+
+
+
+bool TON_O::operator ()(bool IN)
+{
+    int32_t tx;                    /* internal variable */
+
+    /* read system timer */
+    tx = T_PLC_MS();
+
+    // raising Edge
+    if (IN and not M)
+    {
+        ESP_LOGD(TAG, "TON: raising Edge detected");
+        //Start the Timer
+        StartTime = tx;
+    }
+
+    // falling Edge
+    if (not IN and M)
+    {
+        ESP_LOGD(TAG, "TON: falling Edge detected");
+        //reset the Timer
+        StartTime = 0;
+    }
+
+
+    if (IN)
+    {
+        ET = tx - StartTime;
+    }
+    else
+    {
+        ET = 0;
+    }
+
+    M = IN; //remember old State.
+
+    ESP_LOGV(TAG, "ET %i    PT %i", ET, PT);
+    Q = ET >= PT ? true : false;
+    return (Q);
+}
+bool TOF_O::operator ()(bool IN)
+{
+    int32_t tx;                    /* internal variable */
+
+    /* read system timer */
+    tx = T_PLC_MS();
+
+    // raising Edge
+    if (IN and not M)
+    {
+        ESP_LOGD(TAG, "TOF: raising Edge detected");
+        //reset the Timer
+        StartTime = 0;
+    }
+
+    // falling Edge
+    if (not IN and M)
+    {
+        ESP_LOGD(TAG, "TOF: falling Edge detected");
+        //Start the Timer
+        StartTime = tx;
+    }
+
+
+    if (IN)
+    {
+        Q = true;
+        ET = 0;
+    }
+    else
+    {
+        ET = tx - StartTime;
+    }
+
+
+    M = IN; //remember old State.
+
+    ESP_LOGV(TAG, "ET %i    PT %i", ET, PT);
+
+
+    if (ET >= PT)
+    {
+        Q = false;
+    }
+
+
+    return (Q);
+}
+
+bool TOF_1_O::operator ()(bool IN)
+{
+    int32_t tx;                    /* internal variable */
+
+    /* read system timer */
+    tx = T_PLC_MS();
+
+    // raising Edge
+    if (IN and not M)
+    {
+        ESP_LOGD(TAG, "TOF_1: raising Edge detected");
+        //reset the Timer
+        StartTime = 0;
+    }
+
+    // falling Edge
+    if (not IN and M)
+    {
+        ESP_LOGD(TAG, "TOF_1: falling Edge detected");
+        //Start the Timer
+        StartTime = tx;
+    }
+
+
+    if (IN)
+    {
+        Q = true;
+        ET = 0;
+    }
+    else
+    {
+        ET = tx - StartTime;
+    }
+
+
+    M = IN; //remember old State.
+
+    ESP_LOGV(TAG, "ET %i    PT %i", ET, PT);
+
+
+    if (ET >= PT)
+    {
+        Q = false;
+    }
+
+    if (RST)
+    {
+        Q = false;
+        RST = false;
+    }
+
+
+    return (Q);
+}
+
+bool TP_O::operator ()(bool IN)
+{
+    int32_t tx;                    /* internal variable */
+
+    /* read system timer */
+    tx = T_PLC_MS();
+
+    // raising Edge
+    if (IN and not M and not Q)
+    {
+        ESP_LOGD(TAG, "TP: raising Edge detected");
+        //Start the Timer
+        StartTime = tx;
+        Q = true;
+    }
+
+
+
+    if (Q)
+    {
+        ET = tx - StartTime;
+    }
+    else
+    {
+        ET = 0;
+        //reset the Timer
+        StartTime = 0;
+    }
+
+
+    if (ET >= PT)
+    {
+        Q = false;
+    }
+
+    M = IN; //remember old State.
+    return (Q);
+}
+
+
 TOF_R_TRIG_O::TOF_R_TRIG_O(TimerSettings* s, const char * const key)
 	: _subject(s),
 	  _key(key)
@@ -54,36 +241,31 @@ bool TOF_R_TRIG_O::operator ()(bool IN)
         ESP_LOGD(TAG, "TOF_1: raising Edge detected");
         //Start the Timer
         StartTime = tx;
-    }
-
-
-    if (IN)
-    {
         Q = true;
-        ET = 0;
     }
-    else
-    {
-        ET = tx - StartTime;
-    }
+
+    ET = tx - StartTime;
+
 
 
     M = IN; //remember old State.
 
     ESP_LOGV(TAG, "ET %i    PT %i", ET, PT);
 
-
-    if (ET >= PT)
+    if (not IN) //This FB is INPUT First Priority.
     {
-        Q = false;
-    }
+        if (ET >= PT)
+        {
+            Q = false;
+        }
 
-    if (RST)
-    {
-        Q = false;
-        RST = false;
-    }
 
+        if (RST)
+        {
+            Q = false;
+            RST = false;
+        }
+    }
 
     return (Q);
 }
--- StandardLibObserved.h
+++ StandardLibObserved.h
@@ -17,6 +17,120 @@ class Subject;
 
 /**
  * \brief
+    Timer on delay.
+    Q is TRUE, PT milliseconds after IN had a rising edge.
+ *
+ * \param    IN starts timer with rising edge, resets timer with falling edge
+ * \param   PT time to pass, before Q is set
+ * \return    Q is TRUE, PT milliseconds after IN had a rising edge.
+ *
+ */
+class TON_O  : public Observer
+{
+public:
+    TON_O(TimerSettings*, const char * const key);
+    TON_O(const TON_O& a) = delete;
+    TON_O& operator=(const TON_O& other) = delete;
+    virtual ~TON_O();
+
+    virtual void update(Subject*);   // overrides Observer operation
+    //VAR_INPUT
+
+    //VAR_OUTPUT
+    bool Q = false;         /* is TRUE, PT milliseconds after IN had a rising edge */
+    int32_t ET = 0;         /* elapsed time */
+    //call
+    bool operator()(bool IN);/* is TRUE, PT milliseconds after IN had a rising edge */
+private:
+    bool M = false;            /* internal variable */
+    int32_t PT = 0;            /* internal variable */
+    int32_t StartTime = 0;    /* internal variable */
+    TimerSettings* _subject;
+    const char * const _key;
+};
+
+
+/**
+ * \brief
+    Timer of delay.
+    Q is FALSE, PT milliseconds after IN had a falling edge.
+ *
+ * \param    IN starts timer with rising edge, resets timer with falling edge
+ * \param   PT time to pass, before Q is set
+ * \return    Q is FALSE, PT seconds after IN had a falling edge
+ *
+ */
+class TOF_O  : public Observer
+{
+public:
+    TOF_O(TimerSettings*, const char * const key);
+    TOF_O(const TOF_O& a) = delete;
+    TOF_O& operator=(const TOF_O& other) = delete;
+    virtual ~TOF_O();
+
+    virtual void update(Subject*);   // overrides Observer operation
+    //VAR_INPUT
+
+    //VAR_OUTPUT
+    bool Q = false;         /* Q is FALSE, PT milliseconds after IN had a falling edge */
+    int32_t ET = 0;         /* elapsed time */
+    //call
+    bool operator()(bool IN);/* Q is FALSE, PT milliseconds after IN had a falling edge */
+private:
+    bool M = false;            /* internal variable */
+    int32_t PT = 0;            /* internal variable */
+    int32_t StartTime = 0;    /* internal variable */
+    TimerSettings* _subject;
+    const char * const _key;
+};
+
+
+
+/**
+ * \brief
+    Timer of delay.
+    Q is FALSE, PT milliseconds after IN had a falling edge.
+
+
+TOF_1 will extend a pulse on input in for PT seconds.
+in addition the timer can be cleared asynchronously with rst.
+TOF_1 is the same function as TOF from the standard LIB except the asynchronous reset input.
+
+ *
+ * \param    IN starts timer with rising edge, resets timer with falling edge
+ * \param   PT time to pass, before Q is set
+ * \return    Q is FALSE, PT seconds after IN had a falling edge
+ *
+ */
+class TOF_1_O  : public Observer
+{
+public:
+    TOF_1_O(TimerSettings*, const char * const key);
+    TOF_1_O(const TOF_1_O& a) = delete;
+    TOF_1_O& operator=(const TOF_1_O& other) = delete;
+    virtual ~TOF_1_O();
+
+    virtual void update(Subject*);   // overrides Observer operation
+    //VAR_INPUT
+
+    bool RST = false;
+    //VAR_OUTPUT
+    bool Q = false;                    /* Q is FALSE, PT milliseconds after IN had a falling edge */
+    int32_t ET = 0;         /* elapsed time */
+    //call
+    bool operator()(bool IN);/* Q is FALSE, PT milliseconds after IN had a falling edge */
+private:
+    bool M = false;            /* internal variable */
+    int32_t PT = 0;            /* internal variable */
+    int32_t StartTime = 0;    /* internal variable */
+    TimerSettings* _subject;
+    const char * const _key;
+};
+
+
+
+/**
+ * \brief
     Timer of delay.
     Q is FALSE, PT milliseconds after IN had a Rising Edge !!!!!!.
 
@@ -41,6 +155,7 @@ public:
 
     virtual void update(Subject*);   // overrides Observer operation
     //VAR_INPUT
+
     bool RST = false;
     //VAR_OUTPUT
     bool Q = false;                    /* Q is FALSE, PT milliseconds after IN had a falling edge */
@@ -54,6 +169,43 @@ private:
     TimerSettings* _subject;
     const char * const _key;
 };
+
+
+/**
+ * \brief
+    Timer Pulse.
+    Q produces a High-Signal with the length of PT on every rising edge on IN.
+ *
+ * \param    IN Trigger for Start of the Signal
+ * \param   PT The length of the High-Signal in ms
+ * \return    Q is The pulse
+ *
+ */
+class TP_O  : public Observer
+{
+public:
+    TP_O(TimerSettings*, const char * const key);
+    TP_O(const TP_O& a) = delete;
+    TP_O& operator=(const TP_O& other) = delete;
+    virtual ~TP_O();
+
+    virtual void update(Subject*);   // overrides Observer operation
+    //VAR_INPUT
+
+    //VAR_OUTPUT
+    bool Q = false;                    /* The pulse */
+    int32_t ET = 0;         /* The current phase of the High-Signal  */
+    //call
+    bool operator()(bool IN);/* Q is FALSE, PT milliseconds after IN had a falling edge */
+private:
+    bool M = false;            /* internal variable */
+    int32_t PT = 0;            /* internal variable */
+    int32_t StartTime = 0;    /* internal variable */
+    TimerSettings* _subject;
+    const char * const _key;
+};
+
+
 
 
 #endif /* COMPONENTS_PLCLIB_STANDARDLIBOBSERVED_H_ */
