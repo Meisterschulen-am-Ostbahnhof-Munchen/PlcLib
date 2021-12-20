ba485d21cb70d6b38bad16e7141f8d368ed689af
Franz Höpfinger <franz.hoepfinger@ms-muc.de>
2021-12-20 15:26:06 +0100

    add 2 and 3 Timers.

--- TimerSettings3.cpp
+++ TimerSettings3.cpp
@@ -1,36 +1,54 @@
 #include <stdint.h>
 #include "TimerSettings3.h"
 
-TimerSettings3::TimerSettings3() {
-    _PT_up = 1000;
-    _PT_down = 1000;
-    _PT_float = 1000;
-}
+
 
 
 int32_t TimerSettings3::getPT_up(const char* key) const {
-    return (_PT_up);
+    return (_forward->getPT_up(key));
 }
 
 int32_t TimerSettings3::getPT_down(const char* key) const {
-    return (_PT_down);
+    return (_forward->getPT_down(key));
 }
 
 int32_t TimerSettings3::getPT_float(const char* key) const {
-    return (_PT_float);
+    return (_forward->getPT_float(key));
 }
 
 void TimerSettings3::setPT_up(const char* key, int32_t PT_up) {
-    this->_PT_up = PT_up;
+	_forward->setPT_up(key, PT_up);
     notify();
 }
 
 void TimerSettings3::setPT_down(const char* key, int32_t PT_down) {
-    this->_PT_down = PT_down;
+	_forward->setPT_down(key, PT_down);
     notify();
 }
 
 void TimerSettings3::setPT_float(const char* key, int32_t PT_float) {
-    this->_PT_float = PT_float;
+	_forward->setPT_float(key, PT_float);
     notify();
 }
+
+
+TimerSettings3Interface* TimerSettings3::getForward() {
+	return (_forward);
+}
+
+TimerSettings3* TimerSettings3::getInstance() {
+    if (instance == nullptr)
+    {
+        instance = new TimerSettings3();
+    }
+
+    return (instance);
+}
+
+void TimerSettings3::setForward(TimerSettings3Interface *forward) {
+	TimerSettings3::_forward = forward;
+}
+
+
+TimerSettings3Interface* TimerSettings3::_forward = nullptr;
+TimerSettings3* TimerSettings3::instance = nullptr;
